d55370ebb0242f49dc762ceb6d03d9221ab17ce5
Ryan Yang <javathon@gmail.com>
2019-04-12 23:30:59 -0700

    Updates

---
+++ precache-manifest.69583db25265426c5431b5796e181bb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7795724865a9f1a96a69",
+    "url": "/Delivrr/static/css/main.fabfb3fa.chunk.css"
+  },
+  {
+    "revision": "7795724865a9f1a96a69",
+    "url": "/Delivrr/static/js/main.194d76fa.chunk.js"
+  },
+  {
+    "revision": "199a0ad230b8639208f4",
+    "url": "/Delivrr/static/js/runtime~main.49b3f8b6.js"
+  },
+  {
+    "revision": "84cd4971cc0a7647ca63",
+    "url": "/Delivrr/static/js/2.a3206c9c.chunk.js"
+  },
+  {
+    "revision": "37164e91dbb69a7cdd269f7261b8ed2c",
+    "url": "/Delivrr/index.html"
+  }
+];
